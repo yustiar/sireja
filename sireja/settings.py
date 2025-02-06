5ae1862fde51e33e730e20930ad1b66cf43be6e4
yustiar <34302654+yustiar@users.noreply.github.com>
2025-02-06 15:10:56 +0700

    Update settings.py

--- sireja/settings.py
+++ sireja/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-fa%fph5pa)2=j6o^0d!-whu%he1((ni2(8(w2hjkh2&w-i3q21
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 # ALLOWED_HOSTS = ['.vercel.app']
 
 
